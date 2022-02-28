ca4670b91d01394fe517c0ee21137dc56ba4c843
zakhro-yusupova <yu.zakhro01@gmail.com>
2022-02-28 22:25:29 +0500

    Add new styles to results

--- js/main.js
+++ js/main.js
@@ -6,15 +6,40 @@ var elFormBike = document.querySelector(".form__bike-result");
 var elFormCar = document.querySelector(".form__car-result");
 var elFormPlane = document.querySelector(".form__plane-result");
 
-elForm.addEventListener("submit", function(e){
-  e.preventDefault()
+elForm.addEventListener("submit", function(evt){
+  evt.preventDefault()
 
-  var elFormInputVal = elFormInput.value;
+  var elFormInputVal = Number(elFormInput.value.trim());
   var person =3.6;
   var bike =20.1;
   var car =70;
   var plane =800;
 
+  if(elFormInputVal <= 0 || isNaN(elFormInputVal)){
+
+    elFormPerson.textContent = "Raqam kiriting iltimos";
+    elFormBike.textContent = "Raqam kiriting iltimos";
+    elFormCar.textContent = "Raqam kiriting iltimos";
+    elFormPlane.textContent = "Raqam kiriting iltimos";
+
+    elFormPerson.classList.add("result-error");
+    elFormBike.classList.add("result-error");
+    elFormCar.classList.add("result-error");
+    elFormPlane.classList.add("result-error");
+
+    return
+  }
+
+  else{
+    elFormPerson.classList.remove("result-error")
+    elFormPerson.classList.add("result-success");
+    elFormBike.classList.remove("result-error")
+    elFormBike.classList.add("result-success");
+    elFormCar.classList.remove("result-error")
+    elFormCar.classList.add("result-success");
+    elFormPlane.classList.remove("result-error")
+    elFormPlane.classList.add("result-success");
+  }
 
   function onfoot(){
     return elFormInputVal / person;
@@ -34,8 +59,8 @@ elForm.addEventListener("submit", function(e){
     return elFormInputVal / plane;
   }
 
-  elFormPerson.textContent = Math.round (onfoot()) + "soat" ;
-  elFormBike.textContent = Math.round (bybike()) + "soat";
-  elFormCar.textContent = Math.round (bycar()) + "soat";
-  elFormPlane.textContent = Math.round (byplane()) + "soat";
+  elFormPerson.textContent = Math.round (onfoot()) + " soat" ;
+  elFormBike.textContent = Math.round (bybike()) + " soat";
+  elFormCar.textContent = Math.round (bycar()) + " soat";
+  elFormPlane.textContent = Math.round (byplane()) + " soat";
 })
